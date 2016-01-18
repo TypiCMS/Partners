848aee573b93547ab781aa2bb014d6467ca732eb
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:46:50 +0100

    slug unique

--- src/database/migrations/2014_06_15_200602_create_partners_table.php
+++ src/database/migrations/2014_06_15_200602_create_partners_table.php
@@ -14,38 +14,28 @@ class CreatePartnersTable extends Migration
     {
         Schema::create('partners', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
-
             $table->boolean('homepage')->default(0);
             $table->integer('position')->unsigned()->default(1);
             $table->string('image')->nullable();
-
             $table->timestamps();
         });
 
         Schema::create('partner_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('partner_id')->unsigned();
-
             $table->string('locale');
-
             $table->boolean('status')->default(0);
-
             $table->string('title');
             $table->string('slug')->nullable();
             $table->string('website')->nullable();
-
             $table->text('summary');
             $table->text('body');
-
             $table->timestamps();
-
             $table->unique(['partner_id', 'locale']);
+            $table->unique(['locale', 'slug']);
             $table->foreign('partner_id')->references('id')->on('partners')->onDelete('cascade');
-
         });
     }
 
