94876898b67e1136436d21d3bd45dc215382c5f4
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 19:39:28 +0200

    get locales + validation

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -11,9 +11,9 @@ class FormRequest extends AbstractFormRequest
         return [
             'position'  => 'required|integer|min:1',
             'image'     => 'image|max:2000',
-            '*.title'   => 'max:255',
-            '*.slug'    => 'max:255',
-            '*.website' => 'url',
+            'title.*'   => 'max:255',
+            'slug.*'    => 'max:255',
+            'website.*' => 'url',
         ];
     }
 }
