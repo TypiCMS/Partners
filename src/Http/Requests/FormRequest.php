236cae8cb7fabc6ae04b4f02c3656960b8d257e4
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:21:53 +0100

    double arrow alignment removed

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -9,10 +9,10 @@ class FormRequest extends AbstractFormRequest
     public function rules()
     {
         return [
-            'position'  => 'required|integer|min:1',
-            'image'     => 'image|max:2000',
-            'title.*'   => 'max:255',
-            'slug.*'    => 'max:255',
+            'position' => 'required|integer|min:1',
+            'image' => 'image|max:2000',
+            'title.*' => 'max:255',
+            'slug.*' => 'max:255',
             'website.*' => 'url',
         ];
     }
