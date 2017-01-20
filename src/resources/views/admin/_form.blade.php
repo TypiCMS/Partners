2de8639b12e8119808feb1ae36868199430ecde0
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:22:45 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -9,17 +9,17 @@
 @include('core::admin._image-fieldset', ['field' => 'image'])
 
 {!! BootForm::hidden('homepage')->value(0) !!}
-{!! BootForm::checkbox(__('validation.attributes.homepage'), 'homepage') !!}
+{!! BootForm::checkbox(__('Homepage'), 'homepage') !!}
 
 <div class="row">
     <div class="col-sm-2 form-group @if($errors->has('position'))has-error @endif">
-        {!! BootForm::text(__('validation.attributes.position'), 'position') !!}
+        {!! BootForm::text(__('Position'), 'position') !!}
     </div>
 </div>
 
 @include('core::form._title-and-slug')
 {!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('validation.attributes.online'), 'status') !!}
-{!! TranslatableBootForm::text(__('validation.attributes.website'), 'website') !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.body'), 'body')->addClass('ckeditor') !!}
+{!! TranslatableBootForm::checkbox(__('Online'), 'status') !!}
+{!! TranslatableBootForm::text(__('Website'), 'website') !!}
+{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
