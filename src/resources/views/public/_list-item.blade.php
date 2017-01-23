673adacbbc0e2312e9ddc204878d207ee72f52c8
Samuel De Backer <sdebacker@gmail.com>
2017-01-23 19:56:05 +0100

    Front office routes name

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="{{ route($lang.'.partners.slug', $partner->slug) }}" title="{{ $partner->title }}">
+    <a href="{{ route($lang.'::partner', $partner->slug) }}" title="{{ $partner->title }}">
     {{-- <a href="{{ $partner->website }}" title="{{ $partner->title }}" target="_blank"> --}}
         {!! $partner->present()->thumb(null, 200) !!}
     </a>
