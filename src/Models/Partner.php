73ee9acff75144244876f936b3299cf6cecdbec7
Samuel De Backer <sdebacker@gmail.com>
2017-03-06 22:30:32 +0100

    ::class syntax

--- src/Models/Partner.php
+++ src/Models/Partner.php
@@ -6,6 +6,7 @@ use Laracasts\Presenter\PresentableTrait;
 use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\History\Traits\Historable;
+use TypiCMS\Modules\Partners\Presenters\ModulePresenter;
 
 class Partner extends Base
 {
@@ -13,7 +14,7 @@ class Partner extends Base
     use Historable;
     use PresentableTrait;
 
-    protected $presenter = 'TypiCMS\Modules\Partners\Presenters\ModulePresenter';
+    protected $presenter = ModulePresenter::class;
 
     protected $guarded = ['id', 'exit'];
 
