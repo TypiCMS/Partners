16c94f6b11ce979377dac7d55494da96d740f6bb
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:39 +0200

    permissions in config

---
+++ src/config/permissions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'partners' => [
+        'index-partners' => 'Show list',
+        'create-partner' => 'Create',
+        'store-partner' => 'Store',
+        'edit-partner' => 'Edit',
+        'update-partner' => 'Update',
+        'sort-partners' => 'Sort',
+        'destroy-partner' => 'Destroy',
+    ],
+];
