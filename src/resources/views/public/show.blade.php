3f0640a715383cd2c391c2df93f14e47c7380d87
Samuel De Backer <sdebacker@gmail.com>
2019-06-04 14:22:39 +0200

    Update show.blade.php

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -16,7 +16,7 @@
         <p class="partner-website">
             <a class="partner-website-link" href="{{ $model->website }}" target="_blank" rel="noopener noreferrer">{{ $model->website }}</a>
         </p>
-        <p class="partner-summary">{{ nl2br($model->summary) }}</p>
+        <p class="partner-summary">{!! nl2br($model->summary) !!}</p>
         <div class="partner-body">{!! $model->present()->body !!}</div>
     </article>
 
