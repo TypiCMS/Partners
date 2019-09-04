ca8467aec50b84b032f556cf1617232f753cb6dc
Samuel De Backer <sdebacker@gmail.com>
2019-09-04 22:25:58 +0200

    missing parenthesis

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -19,7 +19,7 @@ class ApiController extends BaseApiController
             ->selectFields($request->input('fields.partners'))
             ->allowedSorts(['status_translated', 'position', 'title_translated'])
             ->allowedFilters([
-                AllowedFilter::custom('title', new FilterOr),
+                AllowedFilter::custom('title', new FilterOr()),
             ])
             ->allowedIncludes(['image'])
             ->paginate($request->input('per_page'));
