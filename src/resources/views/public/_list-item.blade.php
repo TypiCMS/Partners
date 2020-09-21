8b1faa3cb2d458740de0ff6e01ef74d14fd065a0
Samuel De Backer <sdebacker@gmail.com>
2020-09-21 22:39:43 +0200

    Update _list-item.blade.php

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,6 +1,6 @@
 <li class="partner-list-item">
     <a class="partner-list-item-link" href="{{ $partner->uri() }}" title="{{ $partner->title }}">
     {{-- <a class="partner-list-item-link" href="{{ $partner->website }}" title="{{ $partner->title }}" target="_blank" rel="noopener noreferrer"> --}}
-        <img class="partner-list-item-image" src="{!! $partner->present()->image(null, 200) !!}" alt="">
+        <img class="partner-list-item-image" src="{{ $partner->present()->image(null, 200) }}" alt="">
     </a>
 </li>
