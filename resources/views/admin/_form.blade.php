2c669931aacb60d8fe7b8be4356160a94dbe2598
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:35:43 +0200

    Forms

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -3,24 +3,32 @@
     <script src="{{ asset('components/ckeditor4/config-full.js') }}"></script>
 @endpush
 
-@component('core::admin._buttons-form', ['model' => $model])
-@endcomponent
-
-{!! BootForm::hidden('id') !!}
-
-<file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
-<file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-
-<div class="mb-3">
-    {!! BootForm::hidden('homepage')->value(0) !!}
-    {!! BootForm::checkbox(__('Homepage'), 'homepage') !!}
+<div class="header">
+    @include('core::admin._button-back', ['url' => $model->indexUrl(), 'title' => __('Partners')])
+    @include('core::admin._title', ['default' => __('New partner')])
+    @component('core::admin._buttons-form', ['model' => $model])
+    @endcomponent
 </div>
 
-@include('core::form._title-and-slug')
-<div class="mb-3">
-    {!! TranslatableBootForm::hidden('status')->value(0) !!}
-    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+<div class="content">
+
+    {!! BootForm::hidden('id') !!}
+
+    <file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
+    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+
+    <div class="mb-3">
+        {!! BootForm::hidden('homepage')->value(0) !!}
+        {!! BootForm::checkbox(__('Homepage'), 'homepage') !!}
+    </div>
+
+    @include('core::form._title-and-slug')
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    </div>
+    {!! TranslatableBootForm::text(__('Website'), 'website')->placeholder('https://') !!}
+    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+
 </div>
-{!! TranslatableBootForm::text(__('Website'), 'website')->placeholder('https://') !!}
-{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,11 +4,6 @@
 
 @section('content')
 
-    <div class="header">
-        @include('core::admin._button-back', ['module' => 'partners'])
-        <h1 class="header-title">@lang('New partner')</h1>
-    </div>
-
     {!! BootForm::open()->action(route('admin::index-partners'))->multipart()->role('form') !!}
         @include('partners::admin._form')
     {!! BootForm::close() !!}
