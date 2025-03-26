ab513c8e648b074fddd191fe4c7f805d19a531ef
Samuel De Backer <sdebacker@gmail.com>
2025-03-26 08:34:36 +0100

    Vue 3

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
     <item-list url-base="/api/partners" fields="id,image_id,position,status,title" table="partners" title="partners" include="image" :exportable="true" :searchable="['title']" :sorting="['position']">
-        <template slot="add-button" v-if="$can('create partners')">
+        <template #add-button v-if="$can('create partners')">
             @include('core::admin._button-create', ['module' => 'partners'])
         </template>
 
-        <template slot="columns" slot-scope="{ sortArray }">
+        <template #columns="{ sortArray }">
             <item-list-column-header name="checkbox" v-if="$can('update partners')||$can('delete partners')"></item-list-column-header>
             <item-list-column-header name="edit" v-if="$can('update partners')"></item-list-column-header>
             <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
@@ -17,7 +17,7 @@
             <item-list-column-header name="title_translated" sortable :sort-array="sortArray" :label="$t('Title')"></item-list-column-header>
         </template>
 
-        <template slot="table-row" slot-scope="{ model, checkedModels, loading }">
+        <template #table-row="{ model, checkedModels, loading }">
             <td class="checkbox" v-if="$can('update partners')||$can('delete partners')">
                 <item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox>
             </td>
