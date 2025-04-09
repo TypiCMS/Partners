b9db4f9b2a09a027ab93b9b4f3a02dd919c14641
Samuel De Backer <sdebacker@gmail.com>
2025-04-09 15:22:53 +0200

    Code style fixed

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -4,6 +4,4 @@ namespace TypiCMS\Modules\Partners\Presenters;
 
 use TypiCMS\Modules\Core\Presenters\Presenter;
 
-class ModulePresenter extends Presenter
-{
-}
+class ModulePresenter extends Presenter {}
