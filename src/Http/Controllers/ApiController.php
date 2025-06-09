a23b56e949b33bec31736d55d4f2d23dd94fb0a8
Samuel De Backer <sdebacker@gmail.com>
2025-06-09 17:40:28 +0200

    QueryBuilder

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -14,8 +14,8 @@ class ApiController extends BaseApiController
 {
     public function index(Request $request): LengthAwarePaginator
     {
-        $data = QueryBuilder::for(Partner::class)
-            ->selectFields()
+        $query = Partner::query()->selectFields();
+        $data = QueryBuilder::for($query)
             ->allowedSorts(['status_translated', 'position', 'title_translated'])
             ->allowedFilters([
                 AllowedFilter::custom('title', new FilterOr()),
