2886e213d97dbcaf2cd8456b35a545ddc185a029
Samuel De Backer <sdebacker@gmail.com>
2025-08-17 18:10:07 +0200

    form attributes changed

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', __('New partner'))
 
 @section('content')
-    {!! BootForm::open()->action(route('admin::index-partners'))->multipart()->role('form') !!}
+    {!! BootForm::open()->action(route('admin::index-partners'))->addClass('main-content') !!}
     @include('partners::admin._form')
     {!! BootForm::close() !!}
 @endsection
